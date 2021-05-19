161a00c08bbeac47d9b94c6754075a6a399e6fd5
Jonathan Li <comm@renderjon.li>
2021-05-18 21:40:48 -0700

    fix: Move card images to tsx

--- src/card.tsx
+++ src/card.tsx
@@ -1,9 +1,39 @@
 import React from "react";
 import { Card } from "./cardList";
 
+import Slime from "./images/Slime.jpg";
+import Fairy from "./images/Fairy.jpg";
+import JrNecki from "./images/JrNecki.jpg";
+import Octopus from "./images/Octopus.jpg";
+import RedSnail from "./images/RedSnail.jpg";
+import WildBoar from "./images/WildBoar.jpg";
+import MagicClaw from "./images/MagicClaw.jpg";
+import RibbonPig from "./images/RibbonPig.jpg";
+import DarkAxeStump from "./images/DarkAxeStump.jpg";
+import GreenMushroom from "./images/GreenMushroom.jpg";
+import OrangeMushroom from "./images/OrangeMushroom.jpg";
+import EmeraldEarrings from "./images/EmeraldEarrings.jpg";
+import Cardback from "./images/cardback.jpg";
+
 interface CardProp {
   card: Card;
 }
+
+const cardImages: Dictionary<any> = {
+  Slime: Slime,
+  Fairy: Fairy,
+  JrNecki: JrNecki,
+  Octopus: Octopus,
+  RedSnail: RedSnail,
+  WildBoar: WildBoar,
+  MagicClaw: MagicClaw,
+  RibbonPig: RibbonPig,
+  DarkAxeStump: DarkAxeStump,
+  GreenMushroom: GreenMushroom,
+  OrangeMushroom: OrangeMushroom,
+  EmeraldEarrings: EmeraldEarrings,
+  Cardback: Cardback,
+};
 
 const miniCardStyle: React.CSSProperties = {
   height: "110px",
@@ -16,7 +46,7 @@ export class ITCGCard extends React.Component<CardProp> {
       <div>
         <img
           style={miniCardStyle}
-          src={this.props.card.image}
+          src={cardImages[this.props.card.image]}
           alt={this.props.card.name}
         />
       </div>
--- src/cardList.ts
+++ src/cardList.ts
@@ -1,83 +1,69 @@
-import Slime from "./images/Slime.jpg";
-import Fairy from "./images/Fairy.jpg";
-import JrNecki from "./images/JrNecki.jpg";
-import Octopus from "./images/Octopus.jpg";
-import RedSnail from "./images/RedSnail.jpg";
-import WildBoar from "./images/WildBoar.jpg";
-import MagicClaw from "./images/MagicClaw.jpg";
-import RibbonPig from "./images/RibbonPig.jpg";
-import DarkAxeStump from "./images/DarkAxeStump.jpg";
-import GreenMushroom from "./images/GreenMushroom.jpg";
-import OrangeMushroom from "./images/OrangeMushroom.jpg";
-import EmeraldEarrings from "./images/EmeraldEarrings.jpg";
-import Cardback from "./images/cardback.jpg";
-
 export interface Card {
   name: string;
-  image: any;
+  image: string;
 }
 
 export const slime: Card = {
   name: "slime",
-  image: Slime,
+  image: "Slime",
 };
 
 export const fairy: Card = {
   name: "fairy",
-  image: Fairy,
+  image: "Fairy",
 };
 
 export const jrnecki: Card = {
   name: "jrnecki",
-  image: JrNecki,
+  image: "JrNecki",
 };
 
 export const octopus: Card = {
   name: "octopus",
-  image: Octopus,
+  image: "Octopus",
 };
 
 export const redsnail: Card = {
   name: "redsnail",
-  image: RedSnail,
+  image: "RedSnail",
 };
 
 export const wildboar: Card = {
   name: "wildboar",
-  image: WildBoar,
+  image: "WildBoar",
 };
 
 export const magicclaw: Card = {
   name: "magicclaw",
-  image: MagicClaw,
+  image: "MagicClaw",
 };
 
 export const ribbonpig: Card = {
   name: "ribbonpig",
-  image: RibbonPig,
+  image: "RibbonPig",
 };
 
 export const darkaxestump: Card = {
   name: "darkaxestump",
-  image: DarkAxeStump,
+  image: "DarkAxeStump",
 };
 
 export const greenmushroom: Card = {
   name: "greenmushroom",
-  image: GreenMushroom,
+  image: "GreenMushroom",
 };
 
 export const orangemushroom: Card = {
   name: "orangemushroom",
-  image: OrangeMushroom,
+  image: "OrangeMushroom",
 };
 
 export const emeraldearrings: Card = {
   name: "emeraldearrings",
-  image: EmeraldEarrings,
+  image: "EmeraldEarrings",
 };
 
 export const cardback: Card = {
   name: "cardback",
-  image: Cardback,
+  image: "Cardback",
 };
