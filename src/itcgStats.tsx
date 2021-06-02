fc089037d25adfeb6c74b06ec78ed17f500ee031
Jonathan Li <comm@renderjon.li>
2021-06-02 16:50:10 -0700

    refactor: Part of itcgCard refactor

--- src/itcgStats.tsx
+++ src/itcgStats.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { PlayerState } from "./game";
+import { ProgressBar } from "./progressBar";
 
 export interface StatProp {
   playerState: PlayerState;
@@ -25,54 +26,15 @@ const levelStyle: React.CSSProperties = {
 
 export class ITCGStats extends React.Component<StatProp> {
   render() {
+    const denom =
+      this.props.playerState.hp > this.props.playerState.maxHP
+        ? this.props.playerState.hp
+        : this.props.playerState.maxHP;
+
     return (
       <div style={statStyle}>
         <div style={levelStyle}>Lv: {this.props.playerState.level}</div>
-        <ProgressBar
-          hp={this.props.playerState.hp}
-          maxHP={this.props.playerState.maxHP}
-        />
-      </div>
-    );
-  }
-}
-
-interface ProgressBarProps {
-  hp: number;
-  maxHP: number;
-}
-
-const barStyle: React.CSSProperties = {
-  background: "linear-gradient(#d3d3d3, 80%, #5c5c5c)",
-  marginLeft: "0.1em",
-  border: "solid",
-  borderRadius: "0.5em",
-  width: "100%",
-  overflow: "hidden",
-};
-
-const defaultFillerStyle: React.CSSProperties = {
-  display: "flex",
-  alignItems: "center",
-  background: "linear-gradient(#ff0000, 80%, #5c5c5c)",
-  transition: "width .2s ease-in",
-  height: "100%",
-  whiteSpace: "nowrap",
-};
-
-class ProgressBar extends React.Component<ProgressBarProps> {
-  render() {
-    const percentage = (this.props.hp / this.props.maxHP) * 100;
-    const fillerStyle = {
-      width: `${percentage}%`,
-      ...defaultFillerStyle,
-    };
-
-    return (
-      <div style={barStyle}>
-        <div style={fillerStyle}>
-          HP:[{this.props.hp}/{this.props.maxHP}]
-        </div>
+        <ProgressBar hp={this.props.playerState.hp} maxHP={denom} />
       </div>
     );
   }
