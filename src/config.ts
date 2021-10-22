dccbe9d03539b1b9947fb92793e5a3d83a18e5b8
Jonathan Li <comm@renderjon.li>
2021-10-22 15:54:07 -0700

    fix: Update thing

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
-export const SERVER = 'http://localhost:18000';
+export const LOCAL_SERVER = 'http://localhost:18000';
+export const SERVER = 'https://server.maple.rs';
 export const USER_COOKIE_NAME = 'user';
 export const GAME_NAME = 'ITCG';
