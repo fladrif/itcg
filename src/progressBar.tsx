1ceef09c5a1e03efd2210b2a01bc2ebd5afbdc65
Jonathan Li <comm@renderjon.li>
2022-03-17 23:56:03 -0700

    feat: Add overheal to progress bar

--- src/progressBar.tsx
+++ src/progressBar.tsx
@@ -14,23 +14,37 @@ const barStyle: React.CSSProperties = {
   overflow: 'hidden',
 };
 
-const defaultFillerStyle: React.CSSProperties = {
-  display: 'flex',
-  alignItems: 'center',
-  background: 'linear-gradient(#ff0000, 80%, #5c5c5c)',
-  transition: 'width .4s ease-in',
-  height: '100%',
-  whiteSpace: 'nowrap',
-};
+function getDefaultFillerStyle(
+  botLimit: number,
+  midLimit: number,
+  topLimit: number
+): React.CSSProperties {
+  return {
+    display: 'flex',
+    alignItems: 'center',
+    background: `linear-gradient(transparent, 80%, #5c5c5c), linear-gradient(90deg, #ff0000 ${botLimit}%, ${midLimit}%, #ff7f00 ${topLimit}%)`,
+    transition: 'width .4s ease-in',
+    height: '100%',
+    whiteSpace: 'nowrap',
+  };
+}
 
 export class ProgressBar extends React.Component<ProgressBarProps> {
   render() {
     const hp = this.props.hp <= 0 ? 0 : this.props.hp;
     const denom = hp > this.props.maxHP ? hp : this.props.maxHP;
     const percentage = (hp / denom) * 100;
+
+    const overHeal = hp > this.props.maxHP ? hp - this.props.maxHP : 0;
+    const overHealPerc = (overHeal / hp) * 100;
+
+    const midLimit = 100 - overHealPerc;
+    const botLimit = midLimit - 2 > 0 ? midLimit - 2 : 0;
+    const topLimit = midLimit + 2 <= 100 ? midLimit + 2 : 100;
+
     const fillerStyle = {
       width: `${percentage}%`,
-      ...defaultFillerStyle,
+      ...getDefaultFillerStyle(botLimit, midLimit, topLimit),
     };
 
     return (
