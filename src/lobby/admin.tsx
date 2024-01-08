d3b20ad0e2ec2cc3193eb7b0b48d96b5d88c9435
Jonathan Li <comm@renderjon.li>
2024-01-07 23:05:43 -0800

    fix: Responsive chart container

--- src/lobby/admin.tsx
+++ src/lobby/admin.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import { LineChart, XAxis, YAxis, Line, Tooltip } from 'recharts';
+import { LineChart, XAxis, YAxis, Line, Tooltip, ResponsiveContainer } from 'recharts';
 import * as d3Array from 'd3-array';
 import * as d3Scale from 'd3-scale';
 import * as d3Time from 'd3-time';
@@ -45,6 +45,21 @@ export class ITCGAdmin extends React.Component<AdminProp, AdminState> {
   async componentDidMount() {
     const data = await this.getPlayerData();
 
+    // const data = [
+    //   { created_at: '2024-01-08T06:59:49.067Z' },
+    //   { created_at: '2024-02-08T06:59:49.067Z' },
+    //   { created_at: '2024-03-08T06:59:49.067Z' },
+    //   { created_at: '2024-04-08T06:59:49.067Z' },
+    //   { created_at: '2024-05-08T06:59:49.067Z' },
+    //   { created_at: '2024-06-08T06:59:49.067Z' },
+    //   { created_at: '2024-07-08T06:59:49.067Z' },
+    //   { created_at: '2024-08-08T06:59:49.067Z' },
+    //   { created_at: '2024-09-08T06:59:49.067Z' },
+    //   { created_at: '2024-10-08T06:59:49.067Z' },
+    //   { created_at: '2024-11-08T06:59:49.067Z' },
+    //   { created_at: '2024-12-08T06:59:49.067Z' },
+    // ];
+
     this.state.playerData = data.map((d) => {
       return { created_at: new Date(d.created_at) };
     });
@@ -59,7 +74,7 @@ export class ITCGAdmin extends React.Component<AdminProp, AdminState> {
       })(this.state.playerData);
 
     const newPlayerData = binnedData.map((b) => {
-      return { created_at: b.x0, num: b.length };
+      return { created_at: b.x0, players: b.length };
     });
 
     return (
@@ -67,12 +82,14 @@ export class ITCGAdmin extends React.Component<AdminProp, AdminState> {
         <h3>
           Users: <span className="badge">{this.state.playerData.length}</span>
         </h3>
-        <LineChart width={1000} height={300} data={newPlayerData}>
-          <XAxis dataKey="created_at" />
-          <YAxis />
-          <Tooltip />
-          <Line type="monotone" dataKey="num" stroke="#8884d8" />
-        </LineChart>
+        <ResponsiveContainer width="95%" height={300}>
+          <LineChart data={newPlayerData}>
+            <XAxis dataKey="created_at" />
+            <YAxis />
+            <Tooltip />
+            <Line type="monotone" dataKey="players" stroke="#8884d8" />
+          </LineChart>
+        </ResponsiveContainer>
       </div>
     );
   }
